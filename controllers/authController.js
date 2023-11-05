392bb19921d6a758231deff9d898b0be6c194b2d
nikhilkops <nikhil.kops@gmail.com>
2023-11-05 18:16:12 +0530

    Removed uncessary imports

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,10 +1,10 @@
 import "express-async-errors";
 import UserModel from "../models/UserModel.js";
 import { UnauthenticatedError } from "../errors/customErros.js";
-import { StatusCodes } from "http-status-codes";
-import { ROLE_TYPE } from "../utils/constants.js";
+import { StatusCodes } from "http-status-codes"; 
 import { encryptPassword, comparePassword } from "../utils/passwordUtils.js";
 import { createJWT } from "../utils/tokenUtils.js";
+
 export const userLogin = async (req, res) => {
   const { email, password } = req.body;
   const user = await UserModel.findOne({ email });
@@ -12,14 +12,7 @@ export const userLogin = async (req, res) => {
   const isValidUser = user && (await comparePassword(password, user.password));
   if (!isValidUser) {
     throw new UnauthenticatedError("Invalid Credentials");
-  }
-  // if (!user) {
-  //   throw new UnauthenticatedError("Invalid Credentials");
-  // }
-
-  // const isPasswordCorrect = await comparePassword(password, user.password);
-  // if (!isPasswordCorrect) throw new UnauthenticatedError("Incorrect Password");
-
+  } 
   user.password = undefined; 
   const token = createJWT({ userId: user._id, role: user.role });
   const oneDay = 1000 * 60 * 60 * 24;
@@ -33,15 +26,12 @@ export const userLogin = async (req, res) => {
 };
 
 export const userSignup = async (req, res) => {
-  const isFirstAccount = (await UserModel.countDocuments()) === 0;
-  const user = req.body;
-  user.role = isFirstAccount ? ROLE_TYPE.ADMIN : ROLE_TYPE.USER;
-
+ 
+  const user = req.body;  
   const hashedPassword = await encryptPassword(user.password);
-  user.password = hashedPassword;
-
+  user.password = hashedPassword; 
   const userCreated = await UserModel.create(user);
-  res.status(StatusCodes.CREATED).json({ message: "User Created" });
+  res.status(StatusCodes.CREATED).json({ message: "User Created",userCreated });
 };
 
 export const userLogout = async(req,res)=>{
--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 import * as dotenv from "dotenv";
 import cors from "cors";
-
 dotenv.config();
 import express from "express";
 const app = express();
@@ -11,22 +10,21 @@ import cookieParser from "cookie-parser";
 import authRouter from "./routes/authRouter.js";
 
 // middleware
-// import errorHandlerMiddleware from "./middlewares/errorHandlerMiddleware.js";
+import errorHandlerMiddleware from "./middlewares/errorHandlerMiddleware.js";
 // import { authenticateUser } from "./middlewares/authMiddleware.js";
-// import {validateTest} from "./middlewares/validationMiddleware.js";
-
-// app.post("/api/v1/test", validateTest, (req, res) => {
-//   const { name } = req.body;
-//   res.json({ name });
-// });
 
 app.use(cors({ origin: "https://foodapp-react-sctz.onrender.com/" }));
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
+app.use(errorHandlerMiddleware);
 app.use(express.json());
 app.use(cookieParser());
+
+// Routes for login logout signup
+app.use("/api/v1/auth", authRouter);
+
 
 app.get("/api/v1/test", (req, res) => {
   const oneDay = 1000 * 60 * 60 * 24;
@@ -36,21 +34,13 @@ app.get("/api/v1/test", (req, res) => {
     secure: true,
   });
   return res.json({ message: "This is a message from test route" });
-});
- 
-app.use("/api/v1/auth", authRouter); 
-// app.use("*", (req, res) => {
-//   res.status(404).json({ msg: `Not Found` });
-// });
-//middleware
-// app.use(errorHandlerMiddleware);
-
-
+}); 
+//middleware 
 app.get("/api/v1/hello", (req, res) => {
-  return res.json({msg:"Hello"});
+  return res.json({ msg: "Hello" });
 });
 
-app.get("/", (req, res,next) => {
+app.get("/", (req, res, next) => {
   const oneDay = 1000 * 60 * 60 * 24;
   res.cookie("Test", "QWERTY", {
     httpOnly: true,
@@ -58,10 +48,13 @@ app.get("/", (req, res,next) => {
     secure: true,
   });
   return res.json({ message: "This is a message from default route" });
- 
 });
-const PORT = process.env.PORT || 1100;
+app.use("*", (req, res) => {
+  res.status(404).json({ msg: `Not Found` });
+});
 
+
+const PORT = process.env.PORT || 1100; 
 try {
   await mongoose.connect(process.env.MONGO_URL);
   app.listen(PORT, () => {
