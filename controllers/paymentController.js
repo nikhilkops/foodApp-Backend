a76550a901ca4080ba53edb72f96c7aa32494a8f
nikhilkops <nikhil.kops@gmail.com>
2023-11-11 02:50:33 -0800

    console currentPrice card object

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -5,6 +5,7 @@ export const checkout = async (req, res) => {
     try {
         const documentId = new mongoose.Types.ObjectId(req.body.id);
         const currentPrice = await PricingModel.findById({ _id: documentId });
+        console.log(currentPrice)
         const price = (Number)(currentPrice.pricing) * 100;
         console.log(typeof price)
         const checkoutOrder = {
