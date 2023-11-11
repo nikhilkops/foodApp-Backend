ff0ddbe83fa79c0d381aca379c14c032ce2a21f7
nikhilkops <nikhil.kops@gmail.com>
2023-11-11 03:04:28 -0800

    change in paymentController

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -4,7 +4,7 @@ import { RazorPayInstance } from '../server.js'
 export const checkout = async (req, res) => {
     try {
         const documentId = new mongoose.Types.ObjectId(req.body.id);
-        const currentPrice = await PricingModel.findById({ _id: documentId });
+        const currentPrice = await PricingModel.findById( documentId);
         console.log(currentPrice)
         const price = (Number)(currentPrice.pricing) * 100;
         console.log(typeof price)
