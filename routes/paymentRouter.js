67a93e96f3b69158b3ca4e4b72fc6993352070fb
nikhilkops <nikhil.kops@gmail.com>
2023-11-11 03:23:41 -0800

    added test in payment

--- routes/paymentRouter.js
+++ routes/paymentRouter.js
@@ -3,6 +3,9 @@ import { checkout } from "../controllers/paymentController.js";
 const router = Router();
 
 router.route("/checkout").get( checkout )
+router.route("/test").get( async(req,res)=>{
+    res.json({msg:"sdnkfnsfk"})
+} )
  
  
 export default router;
