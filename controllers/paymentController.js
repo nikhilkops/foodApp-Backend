b4ef8b04cafa2058e84c5cbffff144d5fdd90323
nikhilkops <nikhil.kops@gmail.com>
2023-11-13 03:16:17 -0800

    Payment Details  stored in DB

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -2,6 +2,7 @@ import * as dotenv from "dotenv";
 dotenv.config();
 import crypto from 'crypto'
 import PricingModel from "../models/PricingModel.js";
+import Checkout from "../models/paymentModel.js";
 import mongoose from "mongoose";
 import { RazorPayInstance } from '../server.js'
 
@@ -9,21 +10,22 @@ export const checkout = async (req, res) => {
     try {
         const documentId = new mongoose.Types.ObjectId(req.params.id);
         const currentPrice = await PricingModel.findById(documentId);
- 
+
         const price = (Number)(currentPrice.pricing) * 100;
         const checkoutOrder = {
             amount: price,
             currency: "INR"
-        } 
+        }
         // pricing: 9999,
         // planType: 'Starter',
 
         const selectedPlan = {
-            planType:currentPrice.planType,
-            price:currentPrice.pricing
-        } 
+            planType: currentPrice.planType,
+            price: currentPrice.pricing
+        }
         const order = await RazorPayInstance.orders.create(checkoutOrder)
         order.cPlan = selectedPlan;
+
 
         res.json(order)
 
@@ -34,7 +36,7 @@ export const checkout = async (req, res) => {
 }
 
 export const paymentVerification = async (req, res) => {
- 
+
     const { razorpay_order_id, razorpay_payment_id, razorpay_signature } = req.body;
     const body = razorpay_order_id + "|" + razorpay_payment_id;
 
@@ -44,15 +46,25 @@ export const paymentVerification = async (req, res) => {
         .digest("hex");
 
     const isAuthentic = expectedSignature === razorpay_signature;
-   
-    if (isAuthentic) {
-        //storein database
-        let VerificationRedirectURL = `https://foodapp-react-sctz.onrender.com/sucessfull?reference=${razorpay_payment_id}`
-        if(process.env.NODE_ENV==='development') VerificationRedirectURL=`http://localhost:3000/sucessfull?reference=${razorpay_payment_id}`
-        res.redirect(VerificationRedirectURL) 
+
+    if (isAuthentic) { 
+        const payment = await RazorPayInstance.payments.fetch(razorpay_payment_id);  
+        const { notes } = payment; 
+        const paymentDetails = {
+            user: notes.userID,
+            product: notes.productID,
+            totalAmount: payment.amount / 100,
+            paymentMethod: payment.method,
+            status: payment.status,
+        } 
+        const paymentDetailsInDB = await Checkout.create(paymentDetails);
+        paymentDetails.payment_id = razorpay_payment_id  
+        let VerificationRedirectURL = `https://foodapp-react-sctz.onrender.com/sucessfull?data=${JSON.stringify(paymentDetails)}`
+        if (process.env.NODE_ENV === 'development') VerificationRedirectURL = `http://localhost:3000/sucessfull?data=${JSON.stringify(paymentDetails)}`
+        res.redirect(VerificationRedirectURL)
     }
 }
 
-export const getKey = (req, res) => {  
+export const getKey = (req, res) => {
     res.json({ key: process.env.RAZORPAY_API_KEY })
 }
---
+++ models/paymentModel.js
@@ -0,0 +1,34 @@
+import mongoose from "mongoose";
+// Define the Checkout Schema
+const checkoutSchema = new mongoose.Schema({
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  product: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'PricingModel',
+    required: true,
+  },
+  totalAmount: {
+    type: Number,
+    required: true,
+  },
+  paymentMethod: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+// Create the Checkout model
+const Checkout = mongoose.model('Checkout', checkoutSchema);
+
+export default Checkout
