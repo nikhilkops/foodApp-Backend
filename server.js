993d94c6e591df20cb38431f204a4a0b848ee126
nikhilkops <nikhil.kops@gmail.com>
2023-11-24 00:11:05 +0530

    Added trust proxy for rete limiter

--- server.js
+++ server.js
@@ -24,6 +24,7 @@ app.use(express.urlencoded({ extended: true }))
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // limit each IP to 100 requests per windowMs
+  trustProxy: true,
 });
 app.use(limiter);
 //CORS
