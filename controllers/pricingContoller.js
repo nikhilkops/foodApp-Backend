6e746d5fd79d1a9ef2bd709570a4550e40b215d0
nikhilkops <nikhil.kops@gmail.com>
2023-11-25 20:13:24 +0530

    changes in get single price

--- controllers/pricingContoller.js
+++ controllers/pricingContoller.js
@@ -1,5 +1,6 @@
+import { BadRequestError } from "../errors/customErros.js";
 import PricingModel from "../models/PricingModel.js"
-
+import mongoose from "mongoose";
 export const getAllPlans = async (req, res) => {
     try {
         const pricingData = await PricingModel.find();
@@ -12,6 +13,9 @@ export const getAllPlans = async (req, res) => {
 export const getSinglePlan = async (req, res) => {
     try {
         const documentId = req.params.id; 
+        const docID_isValid = mongoose.Types.ObjectId.isValid(documentId)
+        if(!docID_isValid) 
+             throw new BadRequestError("Provided docID is not valid !") 
         const result = await PricingModel.findById({ _id: documentId });
         res.json(result)
     }
