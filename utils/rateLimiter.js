eec5003e155d64d15f1cc621f166cae9d4f65853
nikhilkops <nikhil.kops@gmail.com>
2024-03-03 13:30:01 +0530

    Ratelimiter to 50

--- utils/rateLimiter.js
+++ utils/rateLimiter.js
@@ -2,7 +2,7 @@ import rateLimit from 'express-rate-limit';
 
 export const limiter = rateLimit({
     windowMs: 15 * 60 * 1000, // 15 minutes
-    max: 15, // limit each IP to 100 requests per windowMs 
+    max: 50, // limit each IP to 100 requests per windowMs 
     keyGenerator: (req) => {
       // Your custom key generation logic
       return req.headers['x-api-key'] || req.ip;
